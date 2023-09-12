aa80476fd6b2088a859834c7741a530dddc63d56
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-09-11 22:06:08 -0700

    interesting

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    # next number is found by adding up the two numbers before it
+    if length <= 0: 
+        print([])
+        return
+    
+    if length == 1:
+        print([0])
+        return
+    
+    #initialize list with first 2 numbers
+    fib_sequence = [0, 1]
+
+    #dynamically set others
+    for num in range(2, length):
+        next_number = fib_sequence[num - 1] + fib_sequence[num - 2]
+        fib_sequence.append(next_number)
+
+    print(fib_sequence)
